56259797148e399f458bb256c746692b1494d31c
Vortex <vortexsnail12@gmail.com>
2025-03-09 10:24:29 +0000

    setup

---
+++ src/dijkstras.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <stack>
+#include "dijkstras.h"
+
+//struct Edge {
+//    int src=0;
+//    int dst=0;
+//    int weight=0;
+//};
+
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
+{
+
+}
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
+{
+    
+}
+void print_path(const vector<int>& v, int total)
+{
+    
+}
