0a0307e392c967c1e674869cb69f0f18d98e8428
Vortex <vortexsnail12@gmail.com>
2025-03-10 06:18:58 +0000

    try a small cahnge

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -39,7 +39,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             int v = neighbor.src;
             int weight = neighbor.weight;
 
-            if(!visited[v] && distance[u] + weight < distance[v])
+            if(!visited[v] && distance[u] + weight <= distance[v])
             {
                 distance[v] = distance[u] + weight;
                 previous[v] = u;
