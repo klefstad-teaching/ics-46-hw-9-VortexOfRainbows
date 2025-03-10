9f2ae645b398277213273a4c3a214f3246db3076
Vortex <vortexsnail12@gmail.com>
2025-03-10 04:20:26 +0000

    WIP djikstraks

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -12,16 +12,56 @@
 //    int weight=0;
 //};
 
-
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
 {
+    int size = G.numVertices;
+    vector<int> distance{};
+    vector<bool> visited{};
+    distance.resize(size, INF);
+    previous.resize(size, -1);
+    visited.resize(size, false);
 
+    priority_queue<int> pq{};
+    pq.push(source);
+
+    distance[source] = 0; //Distance from source to itself is 0
+
+    while(!pq.empty())
+    {
+        int current = pq.top(); pq.pop();
+        
+        int u = current;
+        if(visited[u])
+            continue;
+        visited[u] = true;
+
+        for (Edge neighbor : G[u])
+        {
+            int v = neighbor.src;
+            int weight = neighbor.weight;
+
+            if(!visited[v] && distance[u] + weight < distance[v])
+            {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push(v);
+            }
+        }
+    }
+    return distance;
 }
-vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
+vector<int> extract_shortest_path(const vector<int>& distances,
+ const vector<int>& previous, 
+ int destination)
 {
     
 }
 void print_path(const vector<int>& v, int total)
 {
-    
+    for(auto i : v)
+    {
+        cout << i << ' ';
+    }
+    cout << endl;
+    cout << "Total cost is " << total << endl;
 }
